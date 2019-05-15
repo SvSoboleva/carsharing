500b4782679afb0fa62757f0da11b1bc9ff59100
Svetlana <sv.soboleva@list.ru>
2019-05-16 00:39:21 +0300

    add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :role, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
+  #devise_for :users 
+
   namespace :v1 do
-    resources :users
+    resources :sessions, only: [:create, :destroy]
   end
 end
